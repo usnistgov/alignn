a77ffd5ba268a08e60d8c6c9dd5f8601d7fbfc03
JARVIS-Unifies <writetokamal.1989@gmail.com>
2021-02-22 15:31:38 -0500

    First commit for working version.

--- main.py
+++ main.py
@@ -1,135 +1,207 @@
-#!pip install jarvis-tools dgl
-# https://docs.dgl.ai/en/0.4.x/tutorials/basics/4_batch.html
-from dgl.data import MiniGCDataset
-import matplotlib.pyplot as plt
-%matplotlib inline
-import networkx as nx
+"""Module to train DGL graph for Atoms."""
+
+# !pip install dgl==0.4.3 jarvis-tools==2021.2.3 
+from torch.utils.data import DataLoader
+from jarvis.db.figshare import data as jdata
+from jarvis.core.atoms import Atoms
+from jarvis.core.atoms import get_supercell_dims
 import dgl
 import torch
-from dgl.nn.pytorch import GraphConv
-import torch.optim as optim
-from torch.utils.data import DataLoader
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
-from jarvis.db.figshare import data
-from jarvis.core.atoms import Atoms
-from jarvis.core.graphs import Graph
-from dgl import DGLGraph
+from torch import optim
+from dgl.nn import GraphConv
+from dgl.nn import AvgPooling
+import numpy as np
+import networkx as nx
+import torch.utils.data
 import torch
-import random
-
-dft_3d = data('dft_3d')
-#dft_2d = data('dft_2d')
-def collate(samples):
-    graphs, labels = map(list, zip(*samples))
-    batched_graph = dgl.batch(graphs)
-    return batched_graph, torch.tensor(labels)
+from sklearn.model_selection import train_test_split
 
 
+config = {
+    "chem_type": "basic",
+    "verbose": True,
+    "use_gp": True,
+    "use_dask": False,
+    "zero_diag": True,
+    "dropout_rate": 0,
+    "lengthscale": 1.0,
+    "variance": 1.0,
+    "bn_size": 2,
+    "gcn_layers": 16,
+    "growth_rate": 24,
+    "fc_layers": 8,
+    "fc_features": 16,
+    "batch_size": 16,
+    "base_lr": 1.0e-2,
+    "chem_type": "basic",
+    "node_atomwise_angle_dist": True,
+    "node_atomwise_rdf": True,
+    "min_cell_size": 5,
+    "get_prim": False,
+    "base_wd": 1.0e-3,
+    "n_epochs": 60,
+    "test_size": 0.2,
+}
 
-class Classifier(nn.Module):
-    def __init__(self, in_dim, hidden_dim, n_classes):
-        super(Classifier, self).__init__()
-        self.conv1 = GraphConv(in_dim, hidden_dim)
-        self.conv2 = GraphConv(hidden_dim, hidden_dim)
-        self.classify = nn.Linear(hidden_dim, n_classes)
+
+def dgl_crystal(atoms, primitive=False, cutoff=8, enforce_c_size=5):
+    """Get DGLGraph from atoms."""
+    g = dgl.DGLGraph()
+    if primitive:
+        atoms = atoms.get_primitive_atoms
+    dim = get_supercell_dims(atoms=atoms, enforce_c_size=enforce_c_size)
+    atoms = atoms.make_supercell(dim)
+    dist = atoms.raw_distance_matrix
+    dist[dist > cutoff] = 0
+    D = nx.Graph(dist)
+    g.from_networkx(D, edge_attrs=["weight"])
+    g.edata["bondlength"] = g.edata["weight"]
+    # del g.edata['weight']
+    g.ndata["atomic_number"] = np.array(atoms.atomic_numbers, dtype=np.int8)
+    return g
+
+
+class SimpleGCN(nn.Module):
+    """Module for simple GCN."""
+
+    def __init__(self, in_features=1, conv_layers=2, width=32):
+        """Initialize class with number og input features, conv layers."""
+        super().__init__()
+        self.layer1 = GraphConv(in_features, width)
+        self.layer2 = GraphConv(width, 1)
+        self.readout = AvgPooling()
 
     def forward(self, g):
-        # Use node degree as the initial node feature. For undirected graphs, the in-degree
-        # is the same as the out_degree.
-        h = g.in_degrees().view(-1, 1).float()
-        # Perform graph convolution and activation function.
-        h = F.relu(self.conv1(g, h))
-        h = F.relu(self.conv2(g, h))
-        g.ndata['h'] = h
-        # Calculate graph representation by averaging all the node representations.
-        hg = dgl.mean_nodes(g, 'h')
-        return self.classify(hg)
+        """Provide forward function."""
+        features = g.ndata["atomic_number"]
+        features = features.view(-1, features.size()[-1]).T
+        x = F.relu(self.layer1(g, features))
+        x = self.layer2(g, x)
+        x = self.readout(g, x)
+        return torch.squeeze(x)
 
 
+class StructureDataset(torch.utils.data.Dataset):
+    """Module for generating DGL dataset."""
 
-class Subset(object):
-    """Subset of a dataset at specified indices
-    Code adapted from PyTorch.
+    def __init__(self, structures, targets):
+        """Initialize the class."""
+        self.graphs = []
+        self.labels = []
+        for i, j in zip(structures, targets):
+            if len(self.labels) < 65:
+                a = Atoms.from_dict(i)
+                # graph=Graph.from_atoms(a).to_networkx()
+                # g1=dgl.from_networkx(graph)
+                # atom_features = "atomic_number"
 
-    Parameters
-    ----------
-    dataset
-        dataset[i] should return the ith datapoint
-    indices : list
-        List of datapoint indices to construct the subset
-    """
-    def __init__(self, dataset, indices):
-        self.dataset = dataset
-        self.indices = indices
+                g2 = dgl_crystal(a)
 
-    def __getitem__(self, item):
-        """Get the datapoint indexed by item
+                self.graphs.append(g2)
+                self.labels.append(j)
 
-        Returns
-        -------
-        tuple
-            datapoint
-        """
-        return self.dataset[self.indices[item]]
+        self.labels = torch.tensor(self.labels)
 
     def __len__(self):
-        """Get subset size
+        """Get length."""
+        return self.labels.shape[0]
 
-        Returns
-        -------
-        int
-            Number of datapoints in the subset
-        """
-        return len(self.indices)
+    def __getitem__(self, idx):
+        """Get item."""
+        return self.graphs[idx], self.labels[idx]
 
-
-
-x=[]
-y=[]
-
-for i in dft_3d:
- if len(x)<500:
-  #g=dgl.from_networkx(Graph.from_atoms(atoms=Atoms.from_dict(i['atoms'])).to_networkx())
-  gg=Graph.from_atoms(atoms=Atoms.from_dict(i['atoms']))
-  dd=DGLGraph()
-
-  dd.add_nodes(len(gg.nodes))
-  dd.add_edges([i[0] for i in gg.edges],[i[1] for i in gg.edges])
-  dd.edata['h']=torch.tensor(gg.edge_attributes)
-  gap=i['optb88vdw_bandgap']
-  if gap>0.05:
-    label=1
-  else:
-    label=0
-  x.append(dd)
-  y.append(label)
+    @staticmethod
+    def collate(samples):
+        """Provide input `samples` is a pair of lists (graphs, labels)."""
+        graphs, labels = map(list, zip(*samples))
+        batched_graph = dgl.batch(graphs)
+        return batched_graph, torch.tensor(labels)
 
 
-
-# Create training and test sets.
-trainset = Subset(x,y)#MiniGCDataset(320, 10, 20)
-testset = Subset(x,y)#MiniGCDataset(80, 10, 20)
-# Use PyTorch's DataLoader and the collate function
-# defined before.
-data_loader = DataLoader(trainset, batch_size=32, shuffle=True,collate_fn=collate)
-
-# Create model
-model = Classifier(1, 256, 2)
-loss_func = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-model.train()
-
-epoch_losses = []
-for epoch in range(80):
-    epoch_loss = 0
-    for iter, (bg, label) in enumerate(data_loader):
-        prediction = model(bg)
-        loss = loss_func(prediction, label)
+def train_epoch(
+    train_loader, model, criterion, optimizer, epoch=0,
+):
+    """Train model."""
+    train_loss = []
+    model.train()
+    for g, target in train_loader:
+        output = model(g)
+        loss = criterion(output, target)
+        # print (loss)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        epoch_loss += loss.detach().item()
-    epoch_loss /= (iter + 1)
-    print('Epoch {}, loss {:.4f}'.format(epoch, epoch_loss))
-    epoch_losses.append(epoch_loss)
+        train_loss.append(loss)
+    return train_loss
+
+
+def evaluate(
+    test_loader, model, criterion, optimizer, epoch=0,
+):
+    """Evaluate model."""
+    test_loss = []
+    model.eval()
+    for g, target in test_loader:
+        output = model(g)
+        loss = criterion(output, target)
+        # print (loss)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        test_loss.append(loss)
+    return test_loss
+
+
+dataset = jdata("dft_3d")
+prop = "optb88vdw_bandgap"
+structures = []
+targets = []
+for i in dataset:
+    if i[prop] != "na":
+        structures.append(i["atoms"])
+        targets.append(i[prop])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    structures, targets, test_size=0.33, random_state=int(37)
+)
+
+train_data = StructureDataset(X_train, y_train)
+val_data = StructureDataset(X_test, y_test)
+
+# use a regular pytorch dataloader
+train_loader = DataLoader(
+    train_data,
+    batch_size=1,
+    shuffle=True,
+    collate_fn=train_data.collate,
+    drop_last=True,
+)
+
+val_loader = DataLoader(
+    val_data,
+    batch_size=1,
+    shuffle=True,
+    collate_fn=val_data.collate,
+    drop_last=True,
+)
+
+model = SimpleGCN()
+criterion = torch.nn.L1Loss()
+optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0)
+
+hist = {"val_loss": [], "train_loss": []}
+t_loss = []
+v_loss = []
+for epoch_idx in range(config["n_epochs"]):
+    train_loss = train_epoch(
+        train_loader, model, criterion, optimizer, epoch=epoch_idx,
+    )
+    val_loss = evaluate(
+        val_loader, model, criterion, optimizer, epoch=epoch_idx
+    )
+    # print (train_loss, type(train_loss),val_loss,type(val_loss))
+    t_loss.append(np.mean(np.array([j.data for j in train_loss])))
+    val_loss = [j.data for j in val_loss]
+    print("t_loss,v_loss", epoch_idx, (train_loss[-1]), val_loss[-1])
