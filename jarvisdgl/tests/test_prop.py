0dc58bce63ed4ecb9c574f372df32e9b949185b2
knc6 <writetokamal.1989@gmail.com>
2021-03-02 11:54:31 -0500

    Temporary.

--- jarvisdgl/tests/test_prop.py
+++ jarvisdgl/tests/test_prop.py
@@ -7,6 +7,27 @@ def test_prop():
     """Test full training run with small batch size."""
     train_property_model(epochs=2, maxrows=16, batch_size=8)
 
+config_d={
+  "dataset": "dft_3d",
+  "target": "formation_energy_peratom",
+  "random_seed": 123,
+  "n_val": 7200,
+  "n_train": 28800,
+  "epochs": 100,
+  "batch_size": 256,
+  "weight_decay": 0,
+  "learning_rate": 0.01,
+  "criterion": "mse",
+  "atom_features": "basic",
+  "optimizer": "adamw",
+  "conv_layers": 3,
+  "edge_features": 16,
+  "node_features": 64,
+  "fc_layers": 1,
+  "fc_features": 128,
+  "output_features": 1,
+  "logscale": False
+}
 
 def test_cgcnn_ignite():
     """Test CGCNN end to end training."""
@@ -17,5 +38,6 @@ def test_cgcnn_ignite():
         n_val=16,
         batch_size=8,
     )
-    result = train_dgl(config)
+    result = train_dgl(config_d)
     print(result)
+test_cgcnn_ignite()
