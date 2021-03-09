d212e4d93433f28a3d9e8e27ce692fbee366d77d
Brian DeCost <brian.decost@nist.gov>
2021-03-09 10:24:46 -0500

    switch from Enum to Literal to clean up some config boilerplate

--- jarvisdgl/config.py
+++ jarvisdgl/config.py
@@ -24,61 +24,7 @@ class BaseSettings(PydanticBaseSettings):
         env_prefix = "jv_"
 
 
-class AutoName(Enum):
-    """Auto string enum.
-
-    https://stackoverflow.com/questions/44781681/how-to-compare-a-string-with-a-python-enum/44785241#44785241
-    """
-
-    def _generate_next_value_(name, start, count, last_values):
-        return name
-
-
-class DatasetEnum(AutoName):
-    """Supported datasets."""
-
-    dft_3d = auto()
-    dft_2d = auto()
-
-
-class TargetEnum(AutoName):
-    """Supported targets."""
-
-    formation_energy_peratom = auto()
-    optb88vdw_bandgap = auto()
-
-
-class FeatureEnum(AutoName):
-    """Supported atom feature sets."""
-
-    basic = auto()
-    atomic_number = auto()
-    cfid = auto()
-    mit = auto()
-
-
-class CriterionEnum(AutoName):
-    """Supported optimizer criteria."""
-
-    mse = auto()
-    l1 = auto()
-
-
 FEATURESET_SIZE = {"basic": 11, "atomic_number": 1, "cfid": 438, "mit": 92}
-
-
-class OptimizerEnum(AutoName):
-    """Supported optimizers."""
-
-    adamw = auto()
-    sgd = auto()
-
-
-class SchedulerEnum(AutoName):
-    """Supported learning rate schedulers."""
-
-    none = auto()
-    onecycle = auto()
 
 
 class CGCNNConfig(BaseSettings):
@@ -123,8 +69,10 @@ class TrainingConfig(BaseSettings):
     version: str = VERSION
 
     # dataset configuration
-    dataset: DatasetEnum = DatasetEnum.dft_3d.value
-    target: TargetEnum = TargetEnum.formation_energy_peratom.value
+    dataset: Literal["dft_3d", "dft_2d"] = "dft_3d"
+    target: Literal[
+        "formation_energy_peratom", "optb88vdw_bandgap"
+    ] = "formation_energy_peratom"
 
     # logging configuration
 
@@ -137,10 +85,10 @@ class TrainingConfig(BaseSettings):
     batch_size: int = 32
     weight_decay: float = 0
     learning_rate: float = 1e-2
-    criterion: CriterionEnum = CriterionEnum.mse.value
-    atom_features: FeatureEnum = FeatureEnum.basic.value
-    optimizer: OptimizerEnum = OptimizerEnum.adamw.value
-    scheduler: SchedulerEnum = SchedulerEnum.onecycle.value
+    criterion: Literal["mse", "l1"] = "mse"
+    atom_features: Literal["basic", "atomic_number", "cfid", "mit"] = "basic"
+    optimizer: Literal["adamw", "sgd"] = "adamw"
+    scheduler: Literal["onecycle", "none"] = "onecycle"
 
     # model configuration
     model: Union[CGCNNConfig, SimpleGCNConfig] = CGCNNConfig(name="cgcnn")
