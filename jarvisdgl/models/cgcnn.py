7b18330da037139c7ee44764a3618954e46081cd
Brian DeCost <brian.decost@nist.gov>
2021-03-10 22:51:42 -0500

    move RBFExpansion to model utils

--- jarvisdgl/models/cgcnn.py
+++ jarvisdgl/models/cgcnn.py
@@ -11,42 +11,7 @@ from dgl.nn import AvgPooling
 from torch import nn
 
 from jarvisdgl.config import CGCNNConfig
-
-
-class RBFExpansion(nn.Module):
-    """Expand interatomic distances with radial basis functions."""
-
-    def __init__(
-        self,
-        vmin: float = 0,
-        vmax: float = 8,
-        bins: int = 40,
-        lengthscale: Optional[float] = None,
-    ):
-        """Register torch parameters for RBF expansion."""
-        super().__init__()
-        self.vmin = vmin
-        self.vmax = vmax
-        self.bins = bins
-        self.register_buffer(
-            "centers", torch.linspace(self.vmin, self.vmax, self.bins)
-        )
-
-        if lengthscale is None:
-            # SchNet-style
-            # set lengthscales relative to granularity of RBF expansion
-            self.lengthscale = np.diff(self.centers).mean()
-            self.gamma = 1 / self.lengthscale
-
-        else:
-            self.lengthscale = lengthscale
-            self.gamma = 1 / (lengthscale ** 2)
-
-    def forward(self, distance: torch.Tensor) -> torch.Tensor:
-        """Apply RBF expansion to interatomic distance tensor."""
-        return torch.exp(
-            -self.gamma * (distance.unsqueeze(1) - self.centers) ** 2
-        )
+from jarvisdgl.models.utils import RBFExpansion
 
 
 class CGCNNConv(nn.Module):
