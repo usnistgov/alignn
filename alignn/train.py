759fd199f1ca40b09744a44b6ef50c3dc3ab9b58
Brian DeCost <brian.decost@nist.gov>
2021-04-23 16:54:30 -0400

    fix python set syntax.

--- alignn/train.py
+++ alignn/train.py
@@ -95,7 +95,7 @@ def train_dgl(
         ignite.utils.manual_seed(config.random_seed)
 
     line_graph = False
-    alignn_models = set("alignn", "dense_alignn")
+    alignn_models = {"alignn", "dense_alignn"}
     if config.model.name == "clgn":
         line_graph = True
     if config.model.name in alignn_models and config.model.alignn_layers > 0:
