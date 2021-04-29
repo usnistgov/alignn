c5e2adce428d55d7bc7d2f50803a521c2835ac59
Kamal Choudhary <writetokamal.1989@gmail.com>
2021-04-29 12:35:27 -0400

    Create utils.py

---
+++ alignn/utils.py
@@ -0,0 +1,14 @@
+"""Shared pydantic settings configuration."""
+from pydantic import BaseSettings as PydanticBaseSettings
+
+
+class BaseSettings(PydanticBaseSettings):
+    """Add configuration to default Pydantic BaseSettings."""
+
+    class Config:
+        """Configure BaseSettings behavior."""
+
+        extra = "forbid"
+        use_enum_values = True
+        env_prefix = "jv_"
+        
