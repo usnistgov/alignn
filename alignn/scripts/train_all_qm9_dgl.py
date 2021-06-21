1859f483ea2b41f6163b845eb689f0f1afdd1e8f
knc6 <writetokamal.1989@gmail.com>
2021-06-21 14:12:48 -0400

    Add QM9_DGL dataset.

---
+++ alignn/scripts/train_all_qm9_dgl.py
@@ -0,0 +1,58 @@
+# from alignn.train_props import train_prop_model
+import os
+from jarvis.tasks.queue_jobs import Queue
+
+# d=data('dft_3d')
+
+props = [
+    "mu",
+    "alpha",
+    "homo",
+    "lumo",
+    "gap",
+    "r2",
+    "zpve",
+    "U0",
+    "U",
+    "H",
+    "G",
+    "Cv",
+    "A",
+    "B",
+    "C",
+]
+# props=['U0','mu']
+cwd_home = os.getcwd()
+for i in props:
+    model_name = "qm9_" + i + "_alignn"
+    model_name = model_name.replace(" ", "")
+    if not os.path.exists(model_name):
+        os.makedirs(model_name)
+    os.chdir(model_name)
+    f = open("train.py", "w")
+    tmp = (
+        "from alignn.train_props import "
+        + 'train_prop_model \ntrain_prop_model(dataset="qm9_dgl",prop="'
+    )
+
+    line = tmp + i + '")\n'
+    f.write(line)
+    f.close()
+    f = open(model_name, "w")
+    job_line = ". ~/.bashrc \nconda activate version \n" + "python train.py\n"
+    submit_cmd = ["sbatch", model_name]
+    directory = os.getcwd()
+    Queue.slurm(
+        job_line=job_line,
+        jobname=model_name,
+        directory=directory,
+        cores=None,
+        submit_cmd=submit_cmd,
+        memory="90G",
+        filename=model_name,
+        queue="singlegpu,general",
+        walltime="150:00:00",
+        pre_job_lines="#SBATCH --gres=gpu:1\n"
+        # pre_job_lines='#SBATCH --gres=gpu:1\n. ~/.bashrc \n'
+    )
+    os.chdir(cwd_home)
