5718456d5e8fc412ebba3fe3cea4b0b882ab3d99
Brian DeCost <brian.decost@nist.gov>
2021-08-25 09:32:58 -0400

    refine learning rate and weight decay

--- alignn/scripts/tune_hypers_fine.py
+++ alignn/scripts/tune_hypers_fine.py
@@ -12,9 +12,21 @@ from ray.tune.schedulers import ASHAScheduler
 
 from alignn.train import train_dgl
 
+# coarse: with ASHA
 # tune learning rate in 1e-4, 1e-2
 # decay in 1e-7, 1e-4
 # tune hidden features in 128, 512?
+
+# finer: full training runs
+# tune learning rate in 3e-4, 1e-2
+# decay in 1e-7, 1e-4
+# tune hidden features in 200, 256, 400
+
+# fine:
+# learning rate: 3e-4, 3e-2
+# decay: 3e-6, 3e-3
+# fix hidden size to 512
+# fix embedding features to 32
 
 config = {
     "random_seed": 123,
@@ -29,8 +41,8 @@ config = {
     "write_checkpoint": True,
     "write_predictions": False,
     "tune": True,
-    "learning_rate": tune.loguniform(3e-4, 1e-2),
-    "weight_decay": tune.loguniform(1e-7, 1e-4),
+    "learning_rate": tune.loguniform(3e-4, 3e-2),
+    "weight_decay": tune.loguniform(3e-6, 3e-3),
     "batch_size": 256,
     "model": {
         "name": "alignn",
@@ -38,8 +50,8 @@ config = {
         "gcn_layers": 2,
         "edge_input_features": 32,
         "triplet_input_features": 32,
-        "embedding_features": tune.choice([32, 64]),
-        "hidden_features": tune.choice([200, 256, 400]),
+        "embedding_features": 32,
+        "hidden_features": 512,
         "link": "identity",
     },
 }
