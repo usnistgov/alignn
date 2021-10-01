9ba5aebc0c4f28e86350fc5b355c48580ac800f7
knc6 <writetokamal.1989@gmail.com>
2021-10-01 12:21:43 -0400

    Fix scikit-learn dependency to work with jarvis-apps, update version.

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="alignn",
-    version="2021.09.29",
+    version="2021.09.30",
     author="Kamal Choudhary, Brian DeCost",
     author_email="kamal.choudhary@nist.gov",
     description="alignn",
     install_requires=[
         "numpy>=1.19.5",
         "scipy>=1.6.3",
-        "jarvis-tools>=2021.09.30",
+        "jarvis-tools>=2021.07.19",
         "torch>=1.7.1",
         "dgl-cu101>=0.6.0",
         "dgl>=0.6.0",
