3d4511a68c3b1cd1dc174f485979686b9500d597
PanayotisManganaris <panos.manganaris@gmail.com>
2022-10-27 09:01:18 -0400

    prepare to transfer to conda-lock

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ https://jarvis.nist.gov.
 
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.org", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
