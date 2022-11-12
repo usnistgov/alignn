4ad1c6c8afc8852464150fba393958d0be8754f5
PanayotisManganaris <panos.manganaris@gmail.com>
2022-11-12 01:35:46 -0500

    linting

--- alignn/tests/test_prop.py
+++ alignn/tests/test_prop.py
@@ -1,11 +1,9 @@
 """Training script test suite."""
 import time
 import matplotlib.pyplot as plt
-import numpy as np
 from alignn.train import train_dgl
 from alignn.pretrained import get_prediction
 from alignn.pretrained import get_multiple_predictions
-from sklearn.metrics import mean_absolute_error
 import os
 from jarvis.core.atoms import Atoms
 
