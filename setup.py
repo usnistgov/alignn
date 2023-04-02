93457c47da093c58f012d971e5e974c4eb34fa1e
knc6 <writetokamal.1989@gmail.com>
2023-04-02 09:58:47 -0400

    Supercon models and version update.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="alignn",
-    version="2023.01.10",
+    version="2023.04.01",
     author="Kamal Choudhary, Brian DeCost",
     author_email="kamal.choudhary@nist.gov",
     description="alignn",
