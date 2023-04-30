78d77c530beb138ee4247c19b9f156e41140f686
knc6 <writetokamal.1989@gmail.com>
2023-04-30 01:01:56 -0400

    Modify stress.

--- alignn/ff/ff.py
+++ alignn/ff/ff.py
@@ -177,7 +177,9 @@ class AlignnAtomwiseCalculator(ase.calculators.calculator.Calculator):
             "stress": full_3x3_to_voigt_6_stress(
                 result["stress"].detach().cpu().numpy()
             )
-            / 160.21766208,
+            * 10.0
+            * num_atoms,
+            # / 160.21766208,
             "dipole": np.zeros(3),
             "charges": np.zeros(len(atoms)),
             "magmom": 0.0,
