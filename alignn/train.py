60344203fa1855c60b4634df88cea68260eb0050
Philipp Benner <philipp@zeroprobability.org>
2023-07-30 09:53:03 +0200

    Fixed compute device in make_standard_scalar_and_pca

--- alignn/train.py
+++ alignn/train.py
@@ -78,12 +78,12 @@ def activated_output_transform(output):
     return y_pred, y
 
 
-def make_standard_scalar_and_pca(output, device):
+def make_standard_scalar_and_pca(output):
     """Use standard scalar and PCS for multi-output data."""
     sc = pk.load(open(os.path.join(tmp_output_dir, "sc.pkl"), "rb"))
     y_pred, y = output
-    y_pred = torch.tensor(sc.transform(y_pred.cpu().numpy()), device=device)
-    y = torch.tensor(sc.transform(y.cpu().numpy()), device=device)
+    y_pred = torch.tensor(sc.transform(y_pred.cpu().numpy()), device=y_pred.device)
+    y = torch.tensor(sc.transform(y.cpu().numpy()), device=y.device)
     # pc = pk.load(open("pca.pkl", "rb"))
     # y_pred = torch.tensor(pc.transform(y_pred), device=device)
     # y = torch.tensor(pc.transform(y), device=device)
