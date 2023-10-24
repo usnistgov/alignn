c28dfb5d57a99e49d04eccd846b2171102a0cfcf
JARVIS-Unifies <writetokamal.1989@gmail.com>
2023-10-24 12:27:30 -0400

    affoct23

--- alignn/scripts/plot_phonons_ff.py
+++ alignn/scripts/plot_phonons_ff.py
@@ -3,9 +3,16 @@
 # from ase.calculators.emt import EMT
 from ase.phonons import Phonons
 import matplotlib.pyplot as plt  # noqa
+from jarvis.core.atoms import Atoms
 
 # from alignn.ff.ff import AlignnAtomwiseCalculator, default_path, ev_curve
-from alignn.ff.ff import AlignnAtomwiseCalculator, phonons, ase_phonon
+from alignn.ff.ff import (
+    AlignnAtomwiseCalculator,
+    phonons,
+    ase_phonon,
+    fd_path,
+    get_figshare_model_ff,
+)
 from jarvis.analysis.structure.spacegroup import (
     Spacegroup3D,
     #    symmetrically_distinct_miller_indices,
@@ -16,8 +23,14 @@ from ase.cell import Cell
 
 
 if __name__ == "__main__":
-    model_path = "/wrk/knc6/ALINN_FC/FD_mult/temp_new"
-    phonons(atoms=atoms, model_path=mlearn, enforce_c_size=3)
+    model_path = get_figshare_model_ff(
+        model_name="aff_Oct23", filename="best_model.pt.epoch41"
+    )  # fd_path() #"/wrk/knc6/ALINN_FC/FD_mult/temp_new"
+    print("model_path", model_path)
+    atoms = Atoms.from_dict(
+        get_jid_data(jid="JVASP-1002", dataset="dft_3d")["atoms"]
+    )
+    phonons(atoms=atoms, model_path=model_path, enforce_c_size=3)
     """
     bs = ase_phonon(
         jid="JVASP-32", ev_file="JVASP-32_ev.png", model_path=model_path
