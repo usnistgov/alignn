0428fe14abaeeb01234af7d8b57592b27e8a190a
knc6 <writetokamal.1989@gmail.com>
2024-04-30 13:16:56 -0400

    Version update.

--- alignn/pretrained.py
+++ alignn/pretrained.py
@@ -6,7 +6,6 @@ import os
 import zipfile
 from tqdm import tqdm
 from alignn.models.alignn import ALIGNN, ALIGNNConfig
-from alignn.dataset import get_torch_dataset
 from torch.utils.data import DataLoader
 import tempfile
 import torch
@@ -340,8 +339,16 @@ def get_multiple_predictions(
     model=None,
     model_name="jv_formation_energy_peratom_alignn",
     print_freq=100,
+    use_lmdb=True,
 ):
     """Use pretrained model on a number of structures."""
+    if use_lmdb:
+        print("Using LMDB dataset.")
+        from alignn.lmdb_dataset import get_torch_dataset
+    else:
+        print("Not using LMDB dataset, memory footprint maybe high.")
+        from alignn.dataset import get_torch_dataset
+
     # import glob
     # atoms_array=[]
     # for i in glob.glob("alignn/examples/sample_data/*.vasp"):
