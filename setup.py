99c533b1d99341641483d15add170951f31fc3c4
Alice0416 <151115442+Alice0416@users.noreply.github.com>
2024-06-22 04:22:19 +0800

    update python version

--- setup.py
+++ setup.py
@@ -53,5 +53,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.8",
 )
